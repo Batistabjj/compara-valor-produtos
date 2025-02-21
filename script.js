026a95cf7356815c8d446e10dfc03b8abe52f72e
Flavio Batista <flaviobatistabjj@gmail.com>
2025-02-20 21:53:45 -0300

    comparador de preço

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function exibirProdutos(produtos) {
     container.innerHTML = "";
 
     // Itera pelos produtos recebidos e exibe no máximo 5 produtos
-    produtos.slice(0, 60).forEach(produto => { 
+    produtos.slice(0, 50).forEach(produto => { 
         // Cria um novo elemento "div" para cada produto
         const item = document.createElement("div");
         
